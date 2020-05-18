ca935a92af8c4e7ecf5391dba0f2ae713d64ad9c
gabyacg29 <g.a.cinquemani@gmail.com>
2020-05-18 08:57:43 -0300

    commit

--- SPRINT2/TIENDA/routes/productos.js
+++ SPRINT2/TIENDA/routes/productos.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var productosCtrl = require('../controllers/productosControllers');
 
 router.get('/', productosCtrl.productosTodos);
-router.get('/detalleProductos', productosCtrl.descripcionProductos);
+router.get('/descripcionProductos', productosCtrl.descripcionProductos);
 router.get('/cargaProductos', productosCtrl.cargarProducto);
 module.exports = router;
